b4806d31e3167a01da0537537066f2db4fa34d92
Adam Roth <adamjroth@gmail.com>
2016-11-24 20:57:56 -0500

    Bump version to 0.10: upgrade to react-native 0.38, update UploadFromCameraRoll sample app.

--- examples/UploadFromCameraRoll/index.ios.js
+++ examples/UploadFromCameraRoll/index.ios.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var React = require('react-native');
-var {
+import React, { Component } from 'react';
+import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  CameraRoll,
-  DeviceEventEmitter,
-  ActivityIndicatorIOS,
   Image,
+  NativeModules,
+  DeviceEventEmitter,
+  ActivityIndicator,
+  CameraRoll,
   TouchableOpacity,
   Modal,
-} = React;
+} from 'react-native';
 
-
-var RNUploader = require('NativeModules').RNUploader;
+var RNUploader = NativeModules.RNUploader;
 
 class UploadFromCameraRoll extends React.Component {
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
-    
+
     this.state = {
       uploading: false,
       showUploadModal: false,
@@ -33,82 +33,83 @@ class UploadFromCameraRoll extends React.Component {
       images: [],
     }
   }
-  
-  componentDidMount(){
-    DeviceEventEmitter.addListener('RNUploaderProgress', (data)=>{
+
+  componentDidMount() {
+    DeviceEventEmitter.addListener('RNUploaderProgress', (data) => {
       let bytesWritten = data.totalBytesWritten;
       let bytesTotal   = data.totalBytesExpectedToWrite;
       let progress     = data.progress;
-      this.setState( { uploadProgress: progress, uploadTotal: bytesTotal, uploadWritten: bytesWritten } );
+      this.setState({uploadProgress: progress, uploadTotal: bytesTotal, uploadWritten: bytesWritten});
     });
   }
 
-  _addImage(){
+  _addImage() {
     const fetchParams = {
-        first: 25,
+      first: 25,
     };
 
-    CameraRoll.getPhotos( fetchParams, (data)=>{
-      const assets = data.edges;
-      const index = parseInt( Math.random() * ( assets.length ) );
-      const randomImage = assets[ index ];
+    CameraRoll.getPhotos(fetchParams).then(
+      (data) => {
+        const assets = data.edges;
+        const index = parseInt(Math.random() * (assets.length));
+        const randomImage = assets[index];
 
-      let images = this.state.images;
-      images.push( randomImage.node.image );
+        let images = this.state.images;
+        images.push(randomImage.node.image);
 
-      this.setState( { images: images } );
+        this.setState({images: images});
     },
-    (err)=>{
+    (err) => {
       console.log(err);
     });
   }
-  
-  _closeUploadModal(){
-    this.setState( { showUploadModal: false, uploadProgress: 0, uploadTotal: 0, uploadWritten: 0, images: [], cancelled: false, } );
+
+  _closeUploadModal() {
+    this.setState({showUploadModal: false, uploadProgress: 0, uploadTotal: 0, uploadWritten: 0, images: [], cancelled: false, });
   }
-  
-  _cancelUpload(){
+
+  _cancelUpload() {
     RNUploader.cancel();
-    this.setState( { uploading: false, cancelled: true } );
+    this.setState({uploading: false, cancelled: true});
   }
-  
-  _uploadImages(){
-    let files = this.state.images.map( (f)=>{
+
+  _uploadImages() {
+    let files = this.state.images.map( (file) => {
       return {
         name: 'file',
         filename: _generateUUID + '.png',
-        filepath: f.uri,
+        filepath: file.uri,
         filetype: 'image/png',
       }
     });
-    
+
     let opts = {
       url: 'https://posttestserver.com/post.php',
-      files: files, 
-      params: { name: 'test-app' }
+      files: files,
+      params: {name: 'test-app'}
     };
-    
-    this.setState({ uploading: true, showUploadModal: true, });    
-    RNUploader.upload( opts, ( err, res )=>{
-      if( err ){
-          console.log(err);
-          return;
+
+    this.setState({ uploading: true, showUploadModal: true, });
+    RNUploader.upload(opts, (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
       }
 
       let status = res.status;
       let responseString = res.data;
 
-      console.log('upload complete with status ' + status);
-      console.log( responseString );
-      this.setState( { uploading: false, uploadStatus: status } );
-    });      
+      console.log('Upload complete with status ' + status);
+      console.log(responseString);
+      this.setState({uploading: false, uploadStatus: status});
+    });
 
   }
-  
-  uploadProgressModal(){
+
+  uploadProgressModal() {
     let uploadProgress;
-    
-    if( this.state.cancelled ){
+
+    if (this.state.cancelled) {
       uploadProgress = (
         <View style={{ margin: 5, alignItems: 'center', }}>
           <Text style={{ marginBottom: 10, }}>
@@ -119,7 +120,7 @@ class UploadFromCameraRoll extends React.Component {
           </TouchableOpacity>
         </View>
       );
-    }else if( !this.state.uploading && this.state.uploadStatus ){
+    } else if (!this.state.uploading && this.state.uploadStatus) {
       uploadProgress = (
         <View style={{ margin: 5, alignItems: 'center', }}>
           <Text style={{ marginBottom: 10, }}>
@@ -130,11 +131,11 @@ class UploadFromCameraRoll extends React.Component {
           </TouchableOpacity>
         </View>
       );
-    }else if( this.state.uploading ){
+    } else if (this.state.uploading) {
       uploadProgress = (
-        <View style={{ alignItems: 'center',  }}>
+        <View style={{ alignItems: 'center', }}>
           <Text style={ styles.title }>Uploading { this.state.images.length } Image{ this.state.images.length == 1 ? '' : 's' }</Text>
-          <ActivityIndicatorIOS
+          <ActivityIndicator
             animating={this.state.animating}
             style={[styles.centering, {height: 80}]}
             size="large" />
@@ -148,51 +149,49 @@ class UploadFromCameraRoll extends React.Component {
         </View>
       );
     }
-    
+
     return uploadProgress;
   }
-  
+
   render() {
-    
     return (
       <View style={styles.container}>
-      
-        <Text style={ styles.title }>
+
+        <Text style={styles.title}>
           react-native-uploader example
         </Text>
 
         <Modal
-          animated={false}
+          animationType={'fade'}
           transparent={false}
-          visible={ this.state.showUploadModal }>
-          
-          <View style={ styles.modal }>
-            { this.uploadProgressModal() }
+          visible={this.state.showUploadModal}>
+
+          <View style={styles.modal}>
+            {this.uploadProgressModal()}
           </View>
-          
+
         </Modal>
-      
-        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', }}>
-          <TouchableOpacity style={ styles.button } onPress={ this._addImage.bind( this ) }>
+
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+          <TouchableOpacity style={styles.button} onPress={this._addImage.bind(this)}>
             <Text>Add Image</Text>
           </TouchableOpacity>
-          
-          <TouchableOpacity style={ styles.button } onPress={ this._uploadImages.bind( this ) }>
+
+          <TouchableOpacity style={styles.button} onPress={this._uploadImages.bind(this)}>
             <Text>Upload</Text>
           </TouchableOpacity>
-          
+
         </View>
-        
-        <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap', }}>
-          { this.state.images.map( (image)=>{
-            return <Image key={ _generateUUID() } source={{ uri: image.uri }} style={ styles.thumbnail } />
+
+        <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
+          {this.state.images.map((image) => {
+            return <Image key={_generateUUID()} source={{uri: image.uri}} style={styles.thumbnail} />
           })}
         </View>
 
       </View>
     );
   }
-
 
 }
 
@@ -242,8 +241,8 @@ var styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-    textAlign: 'center', 
-    fontWeight: '500', 
+    textAlign: 'center',
+    fontWeight: '500',
     fontSize: 14,
   },
   button: {
